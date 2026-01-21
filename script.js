2a261035ae1a1aa11a557d3b27fefc9ad095572e
timur <t.sherov2019@gmail.com>
2026-01-21 18:24:18 +0300

    painting paintings

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 'use strict'
 
+function build_card(){
+  let main=document.createElement("div");
+  main.classList.add("block");
 
+  let Painting=document.createElement("div");
+  Painting.classList.add("painting");
+
+  paint(Painting);
+  let info=document.createElement("div");
+  info.classList.add("info");
+  let button=document.createElement("button");
+  button.innerText="В корзину";
+  
+  let name=document.createElement("p");
+  name.classList.add("name");
+  name.innerText="Name";
+  let price=document.createElement("p");
+  price.innerText=String(rand(10000,100000000))+"$";
+  price.classList.add("price");
+  info.append(name);
+  info.append(price);
+  main.append(Painting);
+  main.append(info);
+  main.append(button);
+  return main;
+}
 
 function rand(min, max) {
     const minCeiled = Math.ceil(min);
@@ -8,20 +33,36 @@ function rand(min, max) {
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
   }
 
-function geherate_picture(){
+function paint(painting){
   let blocks=new Array();
-  for (let i=0;i<rand(1,5);i++){
-    let block;
-    block.x=rand(0,300);
-    block.y=rand(0,500);
-    block.width=rand(50,200);
-    block.height=rand(50,200);
-    block.r=rand(0,255);
-    block.g=rand(0,255);
-    block.b=rand(0,255);
-    block.curve=rand(0,100);
-    block.rotate=rand(0,360);
+  let mozh=600;
+  for (let i=0;i<rand(3,6);i++){
+    let height=rand(10,mozh/2);
+    mozh-=height;
+    let block=document.createElement("div");
+    block.style.position="relative";
+    block.style.left=`${rand(0,200)}px`;
+    
+    block.style.width=`${rand(50,200)}px`;
+    block.style.height=`${height}px`;
+    block.style.backgroundColor=`rgb(${rand(0,255)},${rand(0,255)},${rand(0,255)})`;
+    
+    block.style.borderRadius=rand(0,100);
     blocks.push(block);
   }
 
+  for (let elem of blocks){
+    painting.append(elem);
+  }
+  
+}
+
+
+let main=document.querySelector("main");
+let PaintingsNum= rand(15,21);
+PaintingsNum-=PaintingsNum%3;
+
+for (let i=0;i<PaintingsNum;i++){
+  let card=build_card();
+  main.append(card);
 }
